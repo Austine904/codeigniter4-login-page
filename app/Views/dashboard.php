5853f7e36c1e69e380a654136e78a556ef8cfbba
Austine904 <ochiengaustine89@gmail.com>
2025-03-24 03:27:16 +0300

    css file renaming for base-url

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -4,4 +4,18 @@
             <h1>Hello, <?= session()->get('firstname') ?></h1>
         </div>
     </div>
+    <div class="container mt-5">
+    <form class="p-4 border rounded shadow">
+        <div class="mb-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="email">
+        </div>
+        <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
 </div>
